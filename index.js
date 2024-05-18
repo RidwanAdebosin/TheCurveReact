32f3d8cbed581be1916a85781ac3f0bf9d6f096d
RidwanAdebosin <adebosinadewale@gmail.com>
2024-05-18 21:54:38 +0100

    using CDN for react

--- index.js
+++ index.js
@@ -1,50 +1,121 @@
-  
-      const Aishat = (props) => {
-        const theStyle = {
-            height: "200px",
-            width: "200px",
-            borderRadius: "10px",
-            backgroundColor: props.background,
-            color: props.color,
-            display: "flex",
-            fontSize: "40px",
-            alignItems: "center",
-            justifyContent: "center",
-            margin: "10px"
-        }
-          
-        return (
-            <div style={theStyle}>
-                 <p>{props.text}</p>
-            </div>
-        )
-      };
+// const Aishat = (props) => {
+//   const theStyle = {
+//       height: "200px",
+//       width: "200px",
+//       borderRadius: "10px",
+//       backgroundColor: props.background,
+//       color: props.color,
+//       display: "flex",
+//       fontSize: "40px",
+//       alignItems: "center",
+//       justifyContent: "center",
+//       margin: "10px"
+//   }
 
-      function MyApp() {
-        
-        const wrapperStyle = {
-            justifyContent: "center",
-            marginTop: "100px",
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 200px)",
-            gridTemplateRows: "repeat(2, 200px)",
-            gap: "10px" 
-          };
+//   return (
+//       <div style={theStyle}>
+//            <p>{props.text}</p>
+//       </div>
+//   )
+// };
 
-        return (
-            <div style={wrapperStyle}>
-            <Aishat background="skyblue" color="black" text="1"/>
-            <Aishat background="red" color="white" text="2"/>
-            <Aishat background="purple" color="white" text="3"/>
-            <Aishat background="black" color="white" text="4"/>
-            <Aishat background="orange" color="white" text="5"/>
-            <Aishat background="green" color="white" text="6"/>
-            </div>
-        )
-      }
-    
-      ReactDOM.createRoot(document.getElementById("root")).render(
-        <MyApp/>
-      )
+// function MyApp() {
 
+//   const wrapperStyle = {
+//       justifyContent: "center",
+//       marginTop: "100px",
+//       display: "grid",
+//       gridTemplateColumns: "repeat(3, 200px)",
+//       gridTemplateRows: "repeat(2, 200px)",
+//       gap: "10px"
+//     };
 
+//   return (
+//       <div style={wrapperStyle}>
+//       <Aishat background="skyblue" color="black" text="1"/>
+//       <Aishat background="red" color="white" text="2"/>
+//       <Aishat background="purple" color="white" text="3"/>
+//       <Aishat background="black" color="white" text="4"/>
+//       <Aishat background="orange" color="white" text="5"/>
+//       <Aishat background="green" color="white" text="6"/>
+//       </div>
+//   )
+// }
+
+const SingleContainer = (props) => {
+  const containerStyle = {
+    height: "180px",
+    width: "180px",
+    borderRadius: "5px",
+    backgroundColor: props.background,
+    color: props.color,
+    margin: "5px",
+  };
+
+  const upperContainer = {
+    height: "90px",
+    width: "180px",
+    borderRadius: "5px 5px 0px 0px",
+    backgroundColor: "black",
+  };
+
+  const lowerContainer = {
+    padding: "5px",
+    fontSize: "15px",
+    fontFamily: "sans-serif",
+    fontWeight: "bold",
+  };
+
+  return (
+    <>
+      <div style={containerStyle}>
+        <div style={upperContainer}></div>
+        <div style={lowerContainer}>{props.text}</div>
+      </div>
+    </>
+  );
+};
+
+function MyApp() {
+  const wrapperStyle = {
+    justifyContent: "center",
+    marginTop: "100px",
+    display: "flex",
+    backgroundColor: "#910043",
+    width: "fit-content",
+    padding: "5px",
+  };
+
+  return (
+    <div style={wrapperStyle}>
+      <SingleContainer
+        background="#280034"
+        color="white"
+        text="The Many Media Mutations of Teenage Mutant Ninja Turtles"
+      />
+      <SingleContainer
+        background="#280034"
+        color="white"
+        text="A Day in the Life of TMNT's Foot Clan"
+      />
+      <SingleContainer
+        background="#280034"
+        color="white"
+        text="Saw X Filmmakers on Going Old School With an Untold Chapter for Jigsaw"
+      />
+
+      <SingleContainer
+        background="#280034"
+        color="white"
+        text="Honest Game Trailers | Final Fantasy XVI"
+      />
+
+      <SingleContainer
+        background="#280034"
+        color="white"
+        text="Captain America Sings! How Disney Made Rogers: The Musical a Reality"
+      />
+    </div>
+  );
+}
+ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
