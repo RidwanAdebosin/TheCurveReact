2c0be07912e52ebca4d83bad68f76010e4c32820
RidwanAdebosin <adebosinadewale@gmail.com>
2024-05-18 23:47:58 +0100

    using CDN to create mutliple boxes

--- index.js
+++ index.js
@@ -121,42 +121,99 @@
 //   );
 // }
 
+// const SingleBox = (props) => {
+//   const singleBoxStyle = {
+//     backgroundColor: "#E68622",
+//     height: "40px",
+//     width: "90%",
+//     border: "3px solid orange",
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     borderRadius: "5px",
+//   };
+
+//   return (
+//     <>
+//       <div style={singleBoxStyle}>{props.text}</div>
+//     </>
+//   );
+// };
+
+// function MyApp() {
+//   const boxWrapperStyle = {
+//     display: "flex",
+//     flexDirection: "column",
+//     gap: "10px",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   };
+
+//   return (
+//     <div style={boxWrapperStyle}>
+//       <SingleBox text="Schedule" />
+//       <SingleBox text="Summary" />
+//       <SingleBox text="Recent list" />
+//     </div>
+//   );
+// }
+
 
 const SingleBox = (props) => {
   const singleBoxStyle = {
-    backgroundColor: "#E68622",
-    height: "40px",
-    width:"90%",
-    border: "3px solid orange",
-    display:"flex",
-    alignItems : "center",
-    justifyContent: "center",
-    borderRadius: "5px"
-
-  }
-
-  return (
-    <>
-      <div style = {singleBoxStyle}>{props.text}</div>
-    </>
-  )
-}
-
-function MyApp(){
-  const boxWrapperStyle = {
+    borderRadius: "2px",
+    backgroundColor: props.background,
+    color: "white",
     display: "flex",
-    flexDirection: "column",
-    gap:"10px",
+    fontSize: "20px",
     alignItems: "center",
     justifyContent: "center",
-    
-  }
+  };
+
   return (
-    <div style={boxWrapperStyle}>
-      <SingleBox text ="Schedule"/>
-      <SingleBox text ="Summary"/>
-      <SingleBox text ="Recent list"/>
+    <div style={singleBoxStyle}>
+      <p>{props.text}</p>
     </div>
-  )
+  );
+};
+
+function MyApp() {
+  const boxesWrapperStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(2, 130px)",
+    gridTemplateRows: "repeat(3, 130px)",
+    backgroundColor: "lightgray",
+    gap: "15px",
+    padding: "20px",
+  };
+
+  const boxesWrapperContainerStyle = {
+    display: "flex",
+    gap: "30px",
+    marginTop: "60px",
+    justifyContent: "center",
+  };
+
+  return (
+    <div style={boxesWrapperContainerStyle}>
+      <div style={boxesWrapperStyle}>
+        <SingleBox background="#A93737" text="FrontEnd" />
+        <SingleBox background="#A85237" text="HTML" />
+        <SingleBox background="#3764A8" text="CSS" />
+        <SingleBox background="#C6C946" text="JavaScript" />
+        <SingleBox background="#4837A9" text="React" />
+        <SingleBox background="#37A894" text="DOM" />
+      </div>
+      <div style={boxesWrapperStyle}>
+        <SingleBox background="#A8378F" text="Backend" />
+        <SingleBox background="#3D1412" text="HTML" />
+        <SingleBox background="#233863" text="CSS" />
+        <SingleBox background="#8D7420" text="JavaScript" />
+        <SingleBox background="#16691B" text="Node.js" />
+        <SingleBox background="#370E4A" text="Express" />
+      </div>
+    </div>
+  );
 }
+
 ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
